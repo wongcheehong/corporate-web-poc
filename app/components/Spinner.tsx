6c7fcb9a464122cba9f74d40ce3f302eef776480
Felix Wong Chee Hong <cheehong.wong@ytldigital.com>
2025-04-27 16:24:54 +0800

    feat: add hydratefallback in root

---
+++ app/components/Spinner.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import { cn } from '~/lib/utils';
+import { LoaderCircleIcon } from 'lucide-react';
+
+const spinnerVariants = 'w-16 h-16 rounded-full animate-spin';
+
+interface LoadingSpinnerProps extends React.HTMLAttributes<SVGSVGElement> {
+  className?: string;
+}
+
+const LoadingSpinner = React.forwardRef<SVGSVGElement, LoadingSpinnerProps>(
+  (props, ref) => {
+    const { className, ...rest } = props;
+    return (
+      <LoaderCircleIcon
+        ref={ref}
+        className={cn(spinnerVariants, className)}
+        {...rest}
+      />
+    );
+  },
+);
+
+LoadingSpinner.displayName = 'LoadingSpinner';
+
+export { LoadingSpinner };
